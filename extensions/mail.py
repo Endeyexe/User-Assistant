bf6cb23bd9b3d28c37889e71b9fe32b1e968c83f
Endeyexe <108495952+Endeyexe@users.noreply.github.com>
2022-08-11 15:43:33 -0400

    Booking requests works with messy code

--- extensions/mail.py
+++ extensions/mail.py
@@ -1,4 +1,5 @@
-import hikari, lightbulb
+from urllib import request
+import hikari, lightbulb, miru
 from lightbulb.utils import pag, nav
 import json
 from difflib import get_close_matches
@@ -63,11 +64,104 @@ async def messages(ctx):
         await navigator.run(ctx)
     else: await ctx.respond("**Your inbox is currently empty**")
 
+class BookingRequests(miru.View):
+    def __init__(self, page, requestIDs, requestDetails, requestCount, timeout=None):
+        super().__init__()
+        self.page = page
+        self.requestIDs = requestIDs
+        self.requestDetails = requestDetails
+        self.requestCount = requestCount
+    
+    async def updatePage(self, ctx):
+        if self.requestCount == 0:
+            embed = hikari.Embed(title="No more requests.")
+            await ctx.edit_response(embed, components=[])
+            self.stop()
+            return
+
+        embed = hikari.Embed(title="Booking Requests", description=f"`Request from:` <@{self.requestDetails[self.page][0]}>")
+        embed.add_field("Date", self.requestDetails[self.page][1])
+        embed.add_field("Reason", self.requestDetails[self.page][2])
+        await ctx.edit_response(embed, components=self.build())
+        
+    async def pageLeft(self, ctx):
+        if self.page == 0:
+            self.page = self.requestCount - 1
+        else: 
+            self.page -= 1
+        await self.updatePage(ctx)
+
+    async def pageRight(self, ctx):
+        
+        if self.page == self.requestCount - 1:
+            self.page = 0
+        else:
+            self.page += 1
+        await self.updatePage(ctx)
+
+    @miru.button(emoji="⏪", style=hikari.ButtonStyle.SECONDARY)
+    async def btn_l(self, button: miru.Button, ctx: miru.Context) -> None:
+        await self.pageLeft(ctx)
+        
+    
+    @miru.button(label="Accept", emoji="✅", style=hikari.ButtonStyle.PRIMARY)
+    async def btn_Accept(self, button: miru.Button, ctx: miru.Context) -> None:
+        users = await get_users_data()
+        #add the booking request to the confirmed bookings
+        users[str(ctx.user.id)]["bookings"].update({self.requestIDs[self.page]:self.requestDetails[self.page]})
+        #delete the booking request from the unconfirmed bookings
+        users[str(ctx.user.id)]["booking_requests"].pop(self.requestIDs[self.page])
+        with open("users.json", "w") as f:
+            json.dump(users, f, indent=2)
+        self.page -=1
+        #update values just in case...   
+        self.requestDetails = list(users[str(ctx.user.id)]["booking_requests"].values())
+        self.requestIDs = list(users[str(ctx.user.id)]["booking_requests"].keys())
+        self.requestCount = len(users[str(ctx.user.id)]["booking_requests"])
+        await self.pageRight(ctx)
+
+    @miru.button(label="Deny", emoji="❌", style=hikari.ButtonStyle.PRIMARY)
+    async def btn_Deny(self, button: miru.Button, ctx: miru.Context) -> None:
+        users = await get_users_data()
+        
+        users[str(ctx.user.id)]["booking_requests"].pop(self.requestIDs[self.page])
+        with open("users.json", "w") as f:
+            json.dump(users, f, indent=2)
+        self.page -=1
+        #update values just in case...   
+        self.requestDetails = list(users[str(ctx.user.id)]["booking_requests"].values())
+        self.requestIDs = list(users[str(ctx.user.id)]["booking_requests"].keys())
+        self.requestCount = len(users[str(ctx.user.id)]["booking_requests"])
+        await self.pageRight(ctx)
+
+    @miru.button(emoji="⏩", style=hikari.ButtonStyle.SECONDARY)
+    async def btn_r(self, button: miru.Button, ctx: miru.Context) -> None:
+        await self.pageRight(ctx)
+
+    @miru.button(label="Close", emoji="✅", style=hikari.ButtonStyle.DANGER)
+    async def btn_Close(self, button: miru.Button, ctx: miru.Context) -> None:
+        await ctx.message.delete()
+        self.stop()
+
 @inbox.child
 @lightbulb.command("booking_requests", "View booking requests")
 @lightbulb.implements(lightbulb.SlashSubCommand)
 async def booking_requests(ctx):
-    pass
+    users = await get_users_data()
+    requestCount = len(users[str(ctx.author.id)]["booking_requests"])
+    requestDetails = list(users[str(ctx.author.id)]["booking_requests"].values())
+    requestIDs = list(users[str(ctx.author.id)]["booking_requests"].keys()) 
+    if requestCount > 0:    
+        view = BookingRequests(0, requestIDs, requestDetails, requestCount, timeout=60)
+        embed = hikari.Embed(title="Booking Requests", description=f"`Request from:` <@{requestDetails[0][0]}>")
+        embed.add_field("Date", requestDetails[0][1])
+        embed.add_field("Reason", requestDetails[0][2])
+
+        message = await ctx.respond(embed, components=view.build())
+        message = await message
+        view.start(message)
+        await view.wait()
+    else: await ctx.respond("**You do not have any booking requests currently")
 
 #make a sub sub command group for clear all, clear messages, clear appointments later
 @inbox.child
