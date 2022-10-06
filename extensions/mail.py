6efd51af4b55b04668e7a8dc3e026941920ba5cb
Endeyexe <108495952+Endeyexe@users.noreply.github.com>
2022-10-05 22:39:55 -0400

    there is a bug in here

--- extensions/mail.py
+++ extensions/mail.py
@@ -26,7 +26,7 @@ async def mail(ctx):
     if str(ctx.options.username.id) in users:
         ID = str(uuid.uuid4())[:8]
         #Check if the user has already been messaged by them
-        users[str(ctx.options.username.id)]["messages"].update({ID:[str(ctx.author.id), ctx.options.message]})
+        users[str(ctx.options.username.id)]["messages"].update({ID:[str(ctx.user.id), ctx.options.message]})
         with open("users.json", "w") as f:
             json.dump(users, f, indent=2)
         embed = hikari.Embed(title=f"Message Successfuly Sent ✔️",description=f"Code: {ID}\n To edit or delete this message,\n Use `/message [code] [Action]`") 
@@ -34,35 +34,7 @@ async def mail(ctx):
     else:
         await ctx.respond("**It appears the user you want to message doesn't have a profile setup. A profile can be setup by using bot commands.**")
 
-@plugin.command
-@lightbulb.command("inbox", "Commands specific to the inbox")
-@lightbulb.implements(lightbulb.SlashCommandGroup)
-async def inbox(ctx):
-    pass
 
-@inbox.child
-@lightbulb.command("overview", "overview of your inbox")
-@lightbulb.implements(lightbulb.SlashSubCommand)
-async def overview(ctx):
-    pass
-    #add some general info here about their inbox
-
-@inbox.child
-@lightbulb.command("messages", "View of all messages you've received")
-@lightbulb.implements(lightbulb.SlashSubCommand)
-async def messages(ctx):
-    await set_profile(ctx.author.id)
-    users = await get_users_data()
-
-    inbox = pag.EmbedPaginator()
-    inbox.set_embed_factory(build_embed)
-    if len(users[str(ctx.author.id)]["messages"].keys()): #if they have messages
-        for keys, values in reversed(users[str(ctx.author.id)]["messages"].items()):   
-            inbox.add_line(f"<@{values[0]}> : {values[1]}")
-            inbox.add_line("")
-        navigator = nav.ButtonNavigator(inbox.build_pages())
-        await navigator.run(ctx)
-    else: await ctx.respond("**Your inbox is currently empty**")
 
 class BookingRequests(miru.View):
     def __init__(self, page, requestIDs, requestDetails, requestCount, timeout=None):
@@ -143,38 +115,77 @@ class BookingRequests(miru.View):
         await ctx.message.delete()
         self.stop()
 
-@inbox.child
-@lightbulb.command("booking_requests", "View booking requests")
-@lightbulb.implements(lightbulb.SlashSubCommand)
-async def booking_requests(ctx):
-    users = await get_users_data()
-    requestCount = len(users[str(ctx.author.id)]["booking_requests"])
-    requestDetails = list(users[str(ctx.author.id)]["booking_requests"].values())
-    requestIDs = list(users[str(ctx.author.id)]["booking_requests"].keys()) 
-    if requestCount > 0:    
-        view = BookingRequests(0, requestIDs, requestDetails, requestCount, timeout=60)
-        embed = hikari.Embed(title="Booking Requests", description=f"`Request from:` <@{requestDetails[0][0]}>")
-        embed.add_field("Date", requestDetails[0][1])
-        embed.add_field("Reason", requestDetails[0][2])
+#@inbox.child
+#@lightbulb.command("booking_requests", "View booking requests")
+#@lightbulb.implements(lightbulb.SlashSubCommand)
 
-        message = await ctx.respond(embed, components=view.build())
-        message = await message
-        view.start(message)
-        await view.wait()
-    else: await ctx.respond("**You do not have any booking requests currently")
 
-#make a sub sub command group for clear all, clear messages, clear appointments later
-@inbox.child
-@lightbulb.command("clear_messages", "Clears all messages in your inbox")
-@lightbulb.implements(lightbulb.SlashSubCommand)
-async def clear_messages(ctx):
-    users = await get_users_data()
-    count = len(users[str(ctx.author.id)]["messages"].values())
-    users[str(ctx.author.id)]["messages"].clear()
-    with open("users.json", "w") as f:
-        json.dump(users, f, indent=2)
-    embed = hikari.Embed(title="Inbox has been successfully cleared \✔️", description=f"Cleared {count} messages successfully.")
-    await ctx.respond(embed)
+class inboxes(miru.View):
+    def __init__(self, context ,timeout=None):
+        super().__init__()
+        self.context = context
+    
+    async def messages(self, ctx):
+        await set_profile(ctx.user.id)
+        users = await get_users_data()
+
+        inbox = pag.EmbedPaginator()
+        inbox.set_embed_factory(build_embed)
+        if len(users[str(ctx.user.id)]["messages"].keys()): #this does not work
+            for keys, values in reversed(users[str(ctx.user.id)]["messages"].items()):   
+                inbox.add_line(f"<@{values[0]}> : {values[1]}")
+                inbox.add_line("")
+            navigator = nav.ButtonNavigator(inbox.build_pages())
+            await navigator.run(self.context)
+        else: await ctx.edit_response("**Your inbox is currently empty**")
+
+    async def booking_requests(self, ctx):
+        users = await get_users_data()
+        requestCount = len(users[str(ctx.user.id)]["booking_requests"])
+        requestDetails = list(users[str(ctx.user.id)]["booking_requests"].values())
+        requestIDs = list(users[str(ctx.user.id)]["booking_requests"].keys()) 
+        if requestCount > 0:    
+            view = BookingRequests(0, requestIDs, requestDetails, requestCount, timeout=60)
+            embed = hikari.Embed(title="Booking Requests", description=f"`Request from:` <@{requestDetails[0][0]}>")
+            embed.add_field("Date", requestDetails[0][1])
+            embed.add_field("Reason", requestDetails[0][2])
+            #idk why it only works with original context
+            message = await self.context.respond(embed, components=view.build())
+            message = await message
+            view.start(message)
+            await view.wait()
+        else: await ctx.respond("**You do not have any booking requests currently")
+        
+    @miru.select(
+        placeholder = "Overview",
+        options =[
+            miru.SelectOption(label="Messages"),
+            miru.SelectOption(label="Booking Requests"),
+            miru.SelectOption(label="Booked Requests")
+        ]
+    )
+    async def Inboxes(self, select: miru.Select, ctx: miru.Context) -> None:
+        if select.values[0] == "Messages":
+            await ctx.message.delete()
+            await self.messages(ctx)
+        elif select.values[0] == "Booking Requests":
+            await ctx.message.delete()
+            await self.booking_requests(ctx)
+        elif select.values[0] == "Booked Requests":
+            await ctx.respond("working on it")  
+    
+@plugin.command
+@lightbulb.command("inbox", "Your inbox")
+@lightbulb.implements(lightbulb.SlashCommand)
+async def inbox(ctx):
+    view = inboxes(ctx, timeout=60)
+    embed = hikari.Embed(title="yes")
+    message = await ctx.respond(embed, components=view.build())
+    message = await message
+    view.start(message)
+    await view.wait()
+    #add some general info here about their inbox
+
 
 def load(bot):
     bot.add_plugin(plugin)
